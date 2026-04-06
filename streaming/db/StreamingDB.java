2d0ac5ce23ec654a5a6ac305bfffb544ecb97019
2024118263-gif <2024118263@ufp.edu.pt>
2026-04-06 23:54:09 +0100

    Adiciona metodos de pesquisa e tabela de arquivo no StreamingDB

--- streaming/db/StreamingDB.java
+++ streaming/db/StreamingDB.java
@@ -1,79 +1,107 @@
 package streaming.db;
-
-import streaming.model.Genre;
-import streaming.model.List;
-import java.util.List;
 import streaming.model.Artist;
-import streaming.model.void;
-import HashMap;
-import streaming.model.boolean;
-import RedBlackBST;
-import String;
 import streaming.model.Content;
 import streaming.model.User;
+import edu.princeton.cs.algs4.SeparateChainingHashST;
+import java.util.List;
+
+import java.util.ArrayList;
+import streaming.model.Genre;
+
+
+
 
 public class StreamingDB {
 
-  public HashMap users;
+// 1. Declarar as tabelas privadas da biblioteca algs4
+    private SeparateChainingHashST<String, User> users;
+    private SeparateChainingHashST <String, User> archivedusers;
+    private SeparateChainingHashST<String, Content> contents;
+    private SeparateChainingHashST<String, Artist> artists;
+    private SeparateChainingHashST<String, Genre> genres;
 
-  public HashMap contents;
+    public StreamingDB() {
+        this.users = new SeparateChainingHashST<>();
+        this.contents = new SeparateChainingHashST<>();
+        this.artists = new SeparateChainingHashST<>();
+        this.genres = new SeparateChainingHashST<>();   
+        this.archivedusers = new SeparateChainingHashST<>();
+    }
+    public void addUser(User u) {
+        this.users.put(u.getId(), u);
+    }
 
-  public HashMap artists;
+    public void removeUser(String id) {
+        User userToArchive = this.users.get(id);
+        if (userToArchive != null) {
+            this.archivedusers.put(id, userToArchive);
+            this.users.delete(id);
+        }
+    }
 
-  public HashMap genres;
+    public User getUser(String id) {
+        return this.users.get(id);
+    }
 
-  public RedBlackBST usersByDate;
+    public void addContent(Content c) {
+            this.contents.put(c.getId(), c);
+    }
 
-  public RedBlackBST contentsByYear;
 
-  public RedBlackBST artistsByBirth;
+    public void removeContent(String id) {
+        this.contents.delete(id);
+    }
 
-    public List<Genre> genre;
-    public List<User> user;
-    public List<Artist> artist;
-    public Genre genre;
-    public List<User> user;
-    public List<User> user;
-    public List<Content> content;
-    public List<Artist> artist;
+    public void addArtist(Artist a) {
+      this.artists.put(a.getId(), a);
+    }
 
-  public void addUser(User u) {
-  }
+    public List<User> searchUsersByRegion(String r) {
+        List<User> results = new ArrayList<>();
+        for (String id : this.users.keys()) {
+            User currentUser = this.users.get(id);
+            if (currentUser.getRegion().equals(r)) {
+                results.add(currentUser);
+            }
+        }
+        return results;
+    }
 
-  public void removeUser(String id) {
-  }
+    public List<Content> searchByGenre(Genre g) {
+        List<Content> results = new ArrayList<>();
+        for (String id : this.contents.keys()) {
+            Content currentContent = this.contents.get(id);
+            if (currentContent.getGenre().equals(g)) {
+                results.add(currentContent);
+            }
+        }
+        return results;
+    }
 
-  public User getUser(String id) {
-  return null;
-  }
+    public List<Content> searchByTitle(String sub) {
+        List<Content> results = new ArrayList<>();
+        for (String id : this.contents.keys()) {
+            Content currentContent = this.contents.get(id);
+            if (currentContent.getTitle().toLowerCase().contains(sub.toLowerCase())) {
+                results.add(currentContent);
+            }
+        }
+        return results;
+    }
 
-  public void addContent(Content c) {
-  }
+    public List<Artist> searchArtistsByNationality(String n) {
+        List<Artist> results = new ArrayList<>();
+        for (String id : this.artists.keys()) {
+            Artist currentArtist = this.artists.get(id);
+            if (currentArtist.getNationality().equals(n)) {
+                results.add(currentArtist);
+            }
+        }
+        return results;
+    }
 
-  public void removeContent(String id) {
-  }
-
-  public void addArtist(Artist a) {
-  }
-
-  public List searchUsersByRegion(String r) {
-  return null;
-  }
-
-  public List searchByGenre(Genre g) {
-  return null;
-  }
-
-  public List searchByTitle(String sub) {
-  return null;
-  }
-
-  public List searchArtistsByNationality(String n) {
-  return null;
-  }
-
-  public boolean validateConsistency() {
-  return false;
-  }
+    public boolean validateConsistency() {
+        return false;
+    }
 
 }
